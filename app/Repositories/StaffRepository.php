45a40167f5902708327fdc84e69ed1314e685725
kireal <kiraknax@gmail.com>
2022-06-18 23:19:11 +0300

    front | +

--- app/Repositories/StaffRepository.php
+++ app/Repositories/StaffRepository.php
@@ -15,70 +15,70 @@ use Illuminate\Support\Collection;
  */
 class StaffRepository extends AbstractRepository
 {
-	/**
-	 * @return string
-	 *  Return the model
-	 */
-	public function model()
-	{
-		return Staff::class;
-	}
-
-	public function modelLang()
-	{
-		return StaffLang::class;
-	}
-
-	public function create(array $attributes)
-	{
-		$res = ($entity = $this->makeModel())->fillExisting($attributes)->save();
-		if (!$res) {
-			return null;
-		}
-		$this->fillForCreate($entity, $this->getCurrentLanguage(), $attributes);
-
-		return $entity;
-	}
-
-	private function fillForCreate(Staff $staff, Language $language, array $attributes): void
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
     {
-		$attributes[$staff->getForeignKey()] = $staff->getKey();
-		$attributes[$language->getForeignKey()] = $language->getKey();
-		$entityLang = $this->makeModelLang();
-		$entityLang->fillExisting($attributes)->save();
-	}
+        return Staff::class;
+    }
 
-	public function update(array $attributes, $id)
-	{
-		$isModel = ($id instanceof Model);
-		$model = ($isModel) ? $id : $this->find($id);
-		$model->fillExisting($attributes)->save();
-		/** @var  $entityLang ModelLang */
-		if (!$entityLang = $model->lang) {
-			$this->fillForCreate($model, $this->getCurrentLanguage(), $attributes);
-		} else {
+    public function modelLang()
+    {
+        return StaffLang::class;
+    }
+
+    public function create(array $attributes)
+    {
+        $res = ($entity = $this->makeModel())->fillExisting($attributes)->save();
+        if (!$res) {
+            return null;
+        }
+        $this->fillForCreate($entity, $this->getCurrentLanguage(), $attributes);
+
+        return $entity;
+    }
+
+    private function fillForCreate(Staff $facultie, Language $language, array $attributes): void
+    {
+        $attributes[$facultie->getForeignKey()] = $facultie->getKey();
+        $attributes[$language->getForeignKey()] = $language->getKey();
+        $entityLang = $this->makeModelLang();
+        $entityLang->fillExisting($attributes)->save();
+    }
+
+    public function update(array $attributes, $id)
+    {
+        $isModel = ($id instanceof Model);
+        $model = ($isModel) ? $id : $this->find($id);
+        $model->fillExisting($attributes)->save();
+        /** @var  $entityLang ModelLang */
+        if (!$entityLang = $model->lang) {
+            $this->fillForCreate($model, $this->getCurrentLanguage(), $attributes);
+        } else {
             $entityLang->fillExisting($attributes)->save();
-		}
+        }
 
-		return $model;
-	}
+        return $model;
+    }
 
-	public function getListForAdmin(): LengthAwarePaginator
-	{
-		/** @var  $list */
-		$list = Staff::with('lang')->paginate();
+    public function getListForAdmin(): LengthAwarePaginator
+    {
+        /** @var  $list */
+        $list = Staff::with('lang')->paginate();
 
-		return $list;
-	}
+        return $list;
+    }
 
     /**
      * @param $id
      * @return Staff
      */
-	public function findForEdit($id): Staff
-	{
-		return $this->with('lang')->findOrFail($id);
-	}
+    public function findForEdit($id): Staff
+    {
+        return $this->with('lang')->findOrFail($id);
+    }
 
     public function findAllByFaculty($facultyId): Collection
     {
