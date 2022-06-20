d535c22d76b0356d69a0175ddfa93844889ae67e
kireal <kiraknax@gmail.com>
2022-06-20 15:32:14 +0300

    master | +

--- resources/views/public/layout/includes/footer.blade.php
+++ resources/views/public/layout/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="footer__content">
             <div class="footer__text">
-                Рада Молодих Вчених при ОНУ Мечникова
+                Рада Молодих Вчених при ОНУ
             </div>
         </div>
     </div>
